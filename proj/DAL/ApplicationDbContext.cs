6b27a991a29ea28a42b436d27927827f4697b07a
MaksimKez <156958833+MaksimKez@users.noreply.github.com>
2024-08-11 21:01:30 +0200

    Tests branch (#3)
    * Tests for UserService are done
    
    :)
    
    * Tests are done
    
    and some more fixes

--- proj/DAL/ApplicationDbContext.cs
+++ proj/DAL/ApplicationDbContext.cs
@@ -16,6 +16,7 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions options)
         : base(options)
     { }
+
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
     }
